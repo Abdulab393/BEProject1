c0226a36051be43e0ef7ba7995a3302e2c55a2a2
Abdulab393 <mcrac393@gmail.com>
2023-08-12 17:17:19 +0530

    First commit:Create the structural directory and setup the express server

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express=require('express');
-const app=expresss();
+const app=express();
 const port=8001;
 
 app.listen(port,function(err){
     if(err){
         console.log(`Error on running port:${err}`);
     }
-    console.log(`Server is Running OP on Port:${port}`):
+    console.log(`Server is Running OP on Port:${port}`);
 
 });
